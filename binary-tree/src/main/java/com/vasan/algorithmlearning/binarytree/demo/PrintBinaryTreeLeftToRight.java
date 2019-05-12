77d11bf1044be5abb8baef56ffa4b7c661ecac5d
Vasan Ramani <vasan.ramani@siemens.com>
2019-05-12 00:05:17 -0400

    Print Binary Tree Left to Right

---
+++ binary-tree/src/main/java/com/vasan/algorithmlearning/binarytree/demo/PrintBinaryTreeLeftToRight.java
@@ -0,0 +1,38 @@
+package com.vasan.algorithmlearning.binarytree.demo;
+
+import com.vasan.algorithmlearning.binarytree.model.Node;
+
+public class PrintBinaryTreeLeftToRight {
+  Node data;
+
+  public static void main(String[] args) {
+    PrintBinaryTreeLeftToRight g = new PrintBinaryTreeLeftToRight();
+    g.createData();
+    g.printall();
+    g.printNodeLeftToRight(g.data);
+  }
+
+  private void createData() {
+    this.data = new Node(1, new Node(2, new Node(4, null, null), null),
+        new Node(3, new Node(5, new Node(6, null, null), new Node(7, null, null)),
+            new Node(8, new Node(9, null, null), new Node(10, null, null))));
+  }
+
+  private void printall() {
+    System.out.println(data);
+  }
+
+  private void printNodeLeftToRight(Node data) {
+    if (data.getLeft() == null && data.getRight() == null) {
+      System.out.println(data.getData());
+    }
+
+    if (data.getLeft() != null) {
+      printNodeLeftToRight(data.getLeft());
+    }
+
+    if (data.getRight() != null) {
+      printNodeLeftToRight(data.getRight());
+    }
+  }
+}
---
+++ binary-tree/src/main/java/com/vasan/algorithmlearning/binarytree/model/Node.java
@@ -0,0 +1,28 @@
+package com.vasan.algorithmlearning.binarytree.model;
+
+public class Node {
+  int data;
+  Node left, right;
+
+  public Node(int data, Node left, Node right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+
+  public int getData() {
+    return data;
+  }
+
+  public Node getLeft() {
+    return left;
+  }
+
+  public Node getRight() {
+    return right;
+  }
+
+  public String toString() {
+    return "{" + data + ", " + left + ", " + right + "}";
+  }
+}
